daa93a70282bdbabdd806c8099d90544102440c1
Paul Chon <paul.chon16@hotmail.com>
2019-10-13 03:23:02 -0400

    changed banner, added nav

---
+++ style/js/student.js
@@ -0,0 +1,26 @@
+/*
+* This is the functions for the nav menu
+*/
+
+let overlay = document.getElementById('overlay');
+let closeMenu = document.getElementById('close-menu');
+
+document.getElementById('open-menu').addEventListener('click', function() {
+    overlay.classList.add('show-menu');
+  
+});
+
+document.getElementById('close-menu').addEventListener('click', () => {
+    overlay.classList.remove('show-menu');
+});
+
+
+
+
+
+
+
+
+
+
+
