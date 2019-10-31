0952ae25c0ea32fd37f9e0308bd56622c3789709
Marcelo <marcelo95326@icloud.com>
2019-10-31 17:27:27 -0400

    added ejs
    changed html files to .ejs for express to be able to render them.
    they are located in the views directory.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require ("express");
-
 var app = express();
 
+//Test Route
+app.get('/index', function (req, res) {
+  res.render("index.ejs")
+  console.log("Someone visited the home page")
+});
 
-//testing requests 
-app.get('/hello', function (req, res) {
-  res.send('GET request to the homepage')
-  console.log("Someone visited the site")
+//This is the catch all 
+app.get('*', function (req, res) {
+  res.send("Sorry this directory is not valid, go back to the homepage")
 });
 
 
-app.listen(3000, () => console.log('Server has started!!'));
 
+
+
+app.listen(3000, () => console.log('Server has started!!'));
 //Goto http://localhost:3000/hello   in your browser to see if it works.   Make sure you downloaded node.js  and did npm install express --save first 
