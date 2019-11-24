2dc88482d4c8c2248fcd5e40d9a794170be5d0f1
Paul Chon <paul.chon16@hotmail.com>
2019-11-24 14:30:22 -0500

    WIP class tab

--- assets/js/autoComplete.js
+++ assets/js/autoComplete.js
@@ -1,41 +1,76 @@
-// ! Make school search field appear
-const addSchoolBtn = document.getElementById("add-school");
-const schoolSearchContainer = document.querySelector(".add-school-container");
-
-addSchoolBtn.addEventListener("click", () => {
-    schoolSearchContainer.classList.remove("hidden");
-    schoolSearchContainer.classList.add("active");
-    addSchoolBtn.classList.add("hidden");
-    autocompleter();
-});
-
-// ! AutoSearch
-let schoolData;
-
-async function pullSchools() {
-    const res = await fetch("../data/skoo.json");
-    const data = await res.json();
-
-    return data;
-}
-
-function searchSchool(input) {
-    let matches = schoolData.filter(school => {
-        console.log(school);
-    });
-}
-
-function autocompleter() {
-    let schoolSearchField = document.querySelector(".search-school");
-    // ? Clears input
-    schoolSearchField.value = "";
-    // ? fetches the json for the CUNY schools. Revised as last time it would fetch every time a user typed something in
-    if (schoolData === undefined) {
-        pullSchools().then(res => {
-            schoolData = res;
-        });
+// * Clear input fields on load
+// ? mimics $(document).ready()
+// ? if(document.addEventListener) returns false if there are no addEventListeners on document
+function ready(fn) {
+  if (typeof fn === 'function') {
+    if (document.readyState !== 'loading') {
+      fn()
+    } else if (document.addEventListener) {
+      document.addEventListener('DOMContendLoaded', fn())
+    } else {
+      document.attachEvent('onreadystatechange', () => {
+        if (document.readyState !== 'loading') {
+          fn()
+        }
+      })
     }
-    schoolSearchField.addEventListener("input", () => {
-        searchSchool(schoolSearchField.value);
-    });
+  }
 }
+// * clears the inputfields when refreshing! add class text-field to your input text elements... ** if you need anything to load when DOM load, write it here
+window.ready(() => {
+  const allInputFields = document.querySelectorAll('.text-field')
+  allInputFields.forEach(field => {
+    field.value = ''
+  })
+})
+
+// * Make the school input field appear
+const addSklBtn = document.getElementById('add-school')
+const uiPanel = document.getElementById('school-search-panel')
+
+// * clicking add aggregates the list of schools minus the one that was selected
+
+// ! College Object -- add info
+
+class CollegeProf {
+  constructor(name, code) {
+    this.name = name
+    this.code = code
+  }
+  addMajor(major) {
+    this.major.append(major)
+  }
+
+  // getters
+  getMajors() {
+    this.major.forEach(major => console.log(major))
+  }
+}
+let userColleges = []
+let collegeList
+// * Search field
+const schoolInput = document.querySelector('.school-text-field')
+
+async function colList() {
+  const temp = await fetch('/colleges')
+  const res = await temp.json()
+  return res
+}
+// ? btn to make the school text field to show
+// ? pressing btn also fetchs the database for the CUNY college list
+addSklBtn.addEventListener('click', () => {
+  addSklBtn.style.display = 'none'
+  uiPanel.style.display = 'block'
+  collegeList = colList()
+})
+
+function matchSchool(name) {
+  colList().then(x => console.log(x))
+}
+
+schoolInput.addEventListener('keyup', event => {
+  console.log(schoolInput.value.length)
+  if (schoolInput.value.length >= 3) {
+    matchSchool(schoolInput.value)
+  }
+})
